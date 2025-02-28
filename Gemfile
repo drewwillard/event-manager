282dcc148d0399b1c3ba07636c33654e45a3050a
drewwillard <drew@bcalternative.com>
2025-02-28 11:16:20 -0700

    Basic functionality and google civic api

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem "rubocop"
+
+# gem "rails"
+
+gem "google-apis-civicinfo_v2", "~> 0.24.0"
---
+++ lib/event_manager.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = File.read('keys.yml').strip
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+    legislator_names.join(', ')
+  rescue StandardError
+    'Reps not loaded'
+  end
+end
+
+puts 'Event Manager Initialized!'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+contents.each do |row|
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
+end
